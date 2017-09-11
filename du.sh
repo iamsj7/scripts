feed7c2b6aa27371e6773cd8b2dfaa7cdf88fe02
Shaik Jaleel <shaikjaleel270@gmail.com>
2017-09-11 22:16:37 +0530

    Updated Du

--- du.sh
+++ du.sh
@@ -7,10 +7,10 @@ repo init -u http://github.com/DirtyUnicorns/android_manifest.git -b n7x-caf
 repo sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --force-broken --force-sync -j8
 
 echo "Sync complete Syncing device repos"
-git clone https://github.com/iamsj7/android_device_yu_jalebi -b cm-14.1 device/yu/jalebi
+git clone https://github.com/iamsj7/android_device_yu_jalebi -b du-nx7-caf device/yu/jalebi
 git clone https://github.com/iamsj7/android_vendor_yu_jalebi- -b cm-14.1 vendor/yu/jalebi
 git clone https://github.com/lineageos/android_kernel_cyanogen_msm8916 -b cm-14.1 kernel/cyanogen/msm8916
-git clone https://github.com/iamsj7/android_device_cyanogen_msm8916-common -b cm-14.1 device/cyanogen/msm8916-common
+git clone https://github.com/iamsj7/android_device_cyanogen_msm8916-common -b du-nx7-caf device/cyanogen/msm8916-common
 echo "Sync Complete"
 
 echo "Cloning needed msm8916 hals from du&Los-caf"
@@ -22,6 +22,7 @@ git clone https://github.com/LineageOS/android_hardware_qcom_display -b cm-14.1-
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b cm-14.1-caf-8916 hardware/qcom/media/msm8916
 git clone https://github.com/DirtyUnicorns/android_system_qcom -b n7x-caf system/qcom
 git clone https://github.com/LineageOS/android_external_ant-wireless_antradio-library -b cm-14.1 external/ant-wireless/antradio-library
+git clone https://github.com/LineageOS/hardware_cyanogen -b cm-14.1 hardware/cyanogen
 echo "cloned needed hals from du&los successfully"
 
 echo "Writing compiletime script!!"
