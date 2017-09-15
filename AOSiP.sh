7de1aee44846efe9e779e0d1598850c9038ad910
Shaik Jaleel <shaikjaleel270@gmail.com>
2017-09-15 17:33:32 +0530

    Create AOSiP.sh

---
+++ AOSiP.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Making Directory to sync AOSiP"
+mkdir aosip
+cd aosip
+echo "Preparing to sync"
+repo init -u https://github.com/AOSiP/platform_manifest.git -b nougat-mr2
+repo sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --force-broken --force-sync -j8
+
+echo "Sync complete Syncing device repos"
+git clone https://github.com/iamsj7/android_device_yu_jalebi -b aosip-n device/yu/jalebi
+git clone https://github.com/iamsj7/android_vendor_yu_jalebi- -b cm-14.1 vendor/yu/jalebi
+git clone https://github.com/lineageos/android_kernel_cyanogen_msm8916 -b cm-14.1 kernel/cyanogen/msm8916
+git clone https://github.com/iamsj7/android_device_cyanogen_msm8916-common -b cm-14.1 device/cyanogen/msm8916-common
+echo "Sync Complete"
+
+echo "Cloning needed msm8916 hals from Los-caf"
+rm -rf hardware/qcom/display
+rm -rf hardware/qcom/audio
+rm -rf hardware/qcom/media/
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b cm-14.1-caf-8916 hardware/qcom/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b cm-14.1-caf-8916 hardware/qcom/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b cm-14.1-caf-8916 hardware/qcom/media
+git clone https://github.com/lineageos/android_hardware_cyanogen -b cm-14.1 hardware/cyanogen 
+echo "cloned needed hals from los successfully"
+
+echo "Writing compiletime script!!"
+curl https://raw.githubusercontent.com/LegacyServer/Scripts/master/script_build.sh > script_build.sh
+echo "Writing Script Done"
+
+echo "Bye Script complete"
