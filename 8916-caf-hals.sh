9144dd19125cdc6bd8c3e1b99d8eb923e9d17ebb
Shaik Jaleel <shaikjaleel270@gmail.com>
2017-11-18 05:52:24 +0000

    Create 8916-caf-hals.sh

---
+++ 8916-caf-hals.sh
@@ -0,0 +1,5 @@
+echo "cloning msm8926-caf hals"
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-15.0-caf-8916  hardware/qcom/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-15.0-caf-8916 hardware/qcom/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-15.0-caf-8916  hardware/qcom/media
+echo "done"
