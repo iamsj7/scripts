19e716fabca9db351ba2b6c9850e3e235db1fac8
Shaik Jaleel <shaikjaleel270@gmail.com>
2018-05-19 15:47:06 +0530

    update build script
     https://github.com/LegacyServer/Scripts

--- script_build.sh
+++ script_build.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
 
-# ccache
+# Change As Per Your Need(Script Credits LegacyServer)
+username=iamsj7
+use_ccache="yes"
+make_clean="yes"
+lunch_command="lineage"
+device="$jalebi"
+target_command="$bacon"
+BUILD_USER="iamsj7"
+HOST_NAME="MakeInIndia-SJHub"
+
+# Colors makes things beautiful
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+# CCACHE UMMM!!! Cooks my builds fast
+
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export USE_CCACHE=1
 export CCACHE_DIR=/home/ccache/$username
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
+fi
 
-# clean
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_DIR=/home/ccache/$username
+ccache -C
+export USE_CCACHE=1
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
+
+# Its Clean Time
+if [ "$make_clean" = "yes" ];
+then
 make clean && make clobber
+wait
+echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
+fi
 
-export KBUILD_BUILD_USER=$username
+# Build user and host
+export KBUILD_BUILD_USER=$BUILD_USER
+export KBUILD_BUILD_HOST=$HOST_NAME
 
-export KBUILD_BUILD_HOST=$hostname
-
-# build
+# Build ROM
 . build/envsetup.sh
-lunch $RomlunchCode_$devicecode-userdebug
-make $makecommand -j16
+lunch "$lunch_command"_"$device"-userdebug
+make "$target_command" -j84
