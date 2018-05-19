6bf51f556a46f718e0ff8ed2ffd5058e3b46b63f
Shaik Jaleel <shaikjaleel270@gmail.com>
2018-05-19 16:06:58 +0530

    update hals for lineage-15.1-caf

--- 8916-caf-hals.sh
+++ 8916-caf-hals.sh
@@ -1,5 +1,8 @@
-echo "cloning msm8926-caf hals"
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-15.0-caf-8916  hardware/qcom/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-15.0-caf-8916 hardware/qcom/display
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-15.0-caf-8916  hardware/qcom/media
+echo "cloning msm8916-caf hals from lineageos git"
+rm -rf hardware/qcom/audio-caf/msm8916
+rm -rf hardware/qcom/display-caf/msm8916
+rm -rf hardware/qcom/meidia-caf/msm8916
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-15.1-caf-8916  hardware/qcom/audio-caf/msm8916
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-15.1-caf-8916 hardware/qcom/display-caf/msm8916
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-15.1-caf-8916  hardware/qcom/media-caf/msm8916
 echo "done"
